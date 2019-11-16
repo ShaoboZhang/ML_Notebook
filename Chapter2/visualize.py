9a03971461a6ef1f6b2c233fb47894103d18b430
Shaobo <andyzhang423@gmail.com>
2019-11-16 23:32:00 +0800

    add a line as a compare before upgrading the quality of visualization

--- Chapter2/visualize.py
+++ Chapter2/visualize.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     housing = pd.read_csv('housing.csv')
     housing.hist(bins=50, figsize=(12,8))
+    housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
     housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4, s=housing["population"] / 100,
                  label="population", c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True)
     plt.legend()
