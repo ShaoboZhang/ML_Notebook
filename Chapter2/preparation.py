b444492b57f4aae25f30811d2c32acd09582570f
Shaobo <andyzhang423@gmail.com>
2019-11-21 21:30:12 +0800

    method update

--- Chapter2/preparation.py
+++ Chapter2/preparation.py
@@ -4,6 +4,22 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
 
 
+def data_division():
+    # data stratified
+    housing['income_cat'] = np.ceil(housing['median_income'] / 1.5)
+    housing['income_cat'].where(housing['income_cat'] < 5, 5.0, inplace=True)
+    # stratified splitting
+    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+    strat_train_set = strat_test_set = pd.DataFrame
+    for train_index, test_index in split.split(housing, housing['income_cat']):
+        strat_train_set = housing.loc[train_index]
+        strat_test_set = housing.loc[test_index]
+    # restore data
+    strat_train_set.drop(['income_cat'], axis=1, inplace=True)
+    strat_test_set.drop(['income_cat'], axis=1, inplace=True)
+    return strat_train_set, strat_test_set
+
+
 if __name__ == '__main__':
     housing = pd.read_csv('housing.csv')
     # randomly create train/test set
