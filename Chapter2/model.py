ed0de67cb8064cf5a87e8837e583e3a8e8d6b994
Shaobo <andyzhang423@gmail.com>
2019-11-29 11:23:24 +0800

    model update

--- Chapter2/model.py
+++ Chapter2/model.py
@@ -14,7 +14,7 @@ def display_scores(scores: np.ndarray):
     print('std:  %.3f' % scores.std())
 
 
-def model_selection():
+def model_comparision():
     # data acquisition
     X_train, X_test, y_train, y_test = pipeline()
 
@@ -113,7 +113,7 @@ def grid_search():
 
 
 if __name__ == '__main__':
-    # model_selection()
+    # model_comparision()
 
     # grid_search()
 
